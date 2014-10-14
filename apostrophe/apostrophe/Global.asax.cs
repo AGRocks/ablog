fd5799641d54cf5ceb63f35883de3b949dfa35e2
ag <gabuzaav@gmail.com>
2014-10-14 03:07:44 +0200

    Adding mvc controllers and configuration;

--- apostrophe/apostrophe/Global.asax.cs
+++ apostrophe/apostrophe/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using apostrophe.Config;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace apostrophe
@@ -12,6 +14,8 @@ namespace apostrophe
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
