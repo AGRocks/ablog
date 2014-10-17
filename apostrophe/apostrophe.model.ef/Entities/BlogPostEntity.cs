53b4cbb6f7aadc20bd3ad1d8d44e2ca8102c0687
ag <gabuzaav@gmail.com>
2014-10-18 01:41:51 +0200

    Changes to model. Ninject added.

---
+++ apostrophe/apostrophe.model.ef/Entities/BlogPostEntity.cs
@@ -0,0 +1,14 @@
+﻿using apostrophe.model.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apostrophe.model.ef.Entities
+{
+    public class BlogPostEntity : BlogPost
+    {
+    }
+}
