4c62f4817dd6b83822dff8e3a75a72ec5f8226f5
ag <gabuzaav@gmail.com>
2014-12-16 03:10:48 +0100

    fixed multiplying issue;

--- apostrophe/apostrophe/Scripts/app/controllers/postsController.js
+++ apostrophe/apostrophe/Scripts/app/controllers/postsController.js
@@ -1,9 +1,10 @@
 ﻿'use strict';
 
-apostApp.controller("postsController", function ($scope) {
-    $scope.posts = [
-        { name: "first", text: "per aspera ad astra" },
-        { name: "second", text: "per aspera ad astra" },
-        { name: "third", text: "per aspera ad astra" }
-    ];
-});
+apostApp.controller("postsController",
+    function ($scope) {
+        $scope.posts = [
+            { name: "first", text: "per aspera ad astra" },
+            { name: "second", text: "per aspera ad astra" },
+            { name: "third", text: "per aspera ad astra" }
+        ];
+    });
