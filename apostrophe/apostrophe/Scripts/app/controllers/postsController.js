23a80a256d6b18b9ff9148bb341264445984213f
ag <gabuzaav@gmail.com>
2014-12-16 03:21:47 +0100

    added service mock for posts;

--- apostrophe/apostrophe/Scripts/app/controllers/postsController.js
+++ apostrophe/apostrophe/Scripts/app/controllers/postsController.js
@@ -1,10 +1,6 @@
 ﻿'use strict';
 
 apostApp.controller("postsController",
-    function ($scope) {
-        $scope.posts = [
-            { name: "first", text: "per aspera ad astra" },
-            { name: "second", text: "per aspera ad astra" },
-            { name: "third", text: "per aspera ad astra" }
-        ];
+    function ($scope, postsService) {
+        $scope.posts = postsService.getAllPosts();
     });
---
+++ apostrophe/apostrophe/Scripts/app/services/postsService.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+apostApp.service('postsService', function () {
+    this.getAllPosts = function () {
+        return [
+            { name: "first", text: "per aspera ad astra" },
+            { name: "second", text: "per aspera ad astra" },
+            { name: "third", text: "per aspera ad astra" }
+        ];
+    };
+});
